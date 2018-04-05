8123463a336a23b3f4357b1921cfb2eb2b52e65b
Andrew Dollard <andrewdollard@gmail.com>
2018-04-04 17:58:33 -0700

    wip

--- gists.rb
+++ gists.rb
@@ -1,11 +1,21 @@
 #!/bin/ruby
 
+def print_help
+  puts "help: print this message"
+  puts "sign-up [email address]: sign up a new user"
+end
 
 loop do
   print "> "
-  command = gets.chomp
-  break if command == "quitn"
-  puts "You entered: #{command}"
+  input = gets.chomp.split(' ')
+
+  case input[0]
+  when "quit"
+    break
+  when "help"
+    print_help
+  when "sign-up"
+  end
 end
 
-puts "done."
+puts "goodbye!"
